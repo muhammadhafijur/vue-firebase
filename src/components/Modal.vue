3c91d558f0469873f7c2ff9b2feebb0891fcd881
muhammadhafijur <hafijur.salah@gmail.com>
2023-01-25 15:17:00 +0600

    :sparkles: feat: Slots

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class="backdrop" @click.self="closeModal">
 		<div class="modal" :class="{ sale: theme === 'sale'}">
-			<h1>{{ header }}</h1>
-			<p>{{ text }}</p>
+			<slot>
+				default content
+			</slot>
+			<div class="actions">
+				<slot name="links"></slot>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		props: ['header', 'text', 'theme'],
+		props: ['theme'],
 		methods: {
 			closeModal() {
 				this.$emit('close')
@@ -18,7 +22,7 @@
 	}
 </script>
 
-<style scoped>
+<style>
 
 .modal{
 	width: 400px;
@@ -34,11 +38,27 @@
 	width: 100%;
 	height: 100%;
 }
-h1{
+.modal h1{
 	color: #03cfb4;
 	border: none;
 	padding: 0;
 }
+.modal p{
+	font-style: normal;
+}
+.modal .actions{
+	text-align: center;
+	margin: 30px 0 10px 0;;
+}
+.modal .actions a{
+	color: #333;
+	padding: 8px;
+	border: 1px solid #eee;
+	border-radius: 4px;
+	text-decoration: none;
+	margin: 10px;
+}
+
 
 .modal.sale{
 	background: crimson;
@@ -47,5 +67,12 @@ h1{
 .modal.sale h1{
 	color: white;
 }
+.modal.sale .actions{
+	color: white;
+}
+.modal.sale .actions a{
+	color: white;
+}
+
 
 </style>
